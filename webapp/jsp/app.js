2d015267969b854776134facd03e99cc4c2ca8bc
seulgi90 <seulgi6380@gmail.com>
2022-10-23 18:53:30 +0900

    bitcamp-study

---
+++ webapp/jsp/app.js
@@ -0,0 +1,13 @@
+const player = {
+  name: "nico",
+  sayHello: function (otherPersonName) {
+    console.log("hello");
+  },
+};
+
+function sayHello() {
+  console.log("he")
+}
+
+console.log(player.name);
+player.sayHello();
